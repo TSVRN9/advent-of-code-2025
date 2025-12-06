2b2d1b0c03dd936fe055a17142943daa3a133882
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2025-12-06 13:43:02 -0500

    day 5

---
+++ day5/part2.py
@@ -0,0 +1,31 @@
+with open("day5/input.txt", "r") as f:
+    content = f.read()
+    sections = content.split("\n\n")
+
+    ranges = [tuple(map(int, l.split("-"))) for l in sections[0].splitlines()]
+    # ids = [int(l) for l in sections[1].splitlines()]
+
+    bounds = [(b[0], 1) for b in ranges] + [(b[1], -1) for b in ranges]
+    bounds = sorted(bounds, key=lambda r: r[0])
+
+    open_counts: list[tuple[int, int]] = []
+    parens = 0
+
+    for bound, delta in bounds:
+        parens += delta
+        open_counts.append((bound, parens))
+
+    print(open_counts)
+    
+    num_fresh = 0
+    for b, prev in zip(open_counts[1:], open_counts):
+        print(f'b={b}, prev={prev}')
+        if prev[1] > 0:
+            delta = b[0] - prev[0]
+            if b[1] == 0:
+                delta += 1
+            num_fresh += delta
+            print(f'delta={delta}')
+        
+    
+    print(num_fresh)
