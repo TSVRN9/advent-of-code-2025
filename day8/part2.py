88c664c9b1e4ecc4d6ee1843f4e1f6bb195a9438
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2025-12-08 19:58:22 -0500

    day 8

---
+++ day8/part2.py
@@ -0,0 +1,35 @@
+import numpy as np
+from collections import defaultdict as D
+from scipy.spatial.distance import pdist
+from math import prod
+
+points = np.loadtxt("day8/input.txt", dtype=np.intp, delimiter=",")
+n = len(points)
+distances = pdist(points)
+sorted_indices = np.argsort(distances)
+index_pairs = np.array(np.triu_indices(n, k=1)).T
+sorted_pairs = index_pairs[sorted_indices]
+
+circuits: D[int, set[int]] = D(set)
+
+for a, b in sorted_pairs:
+    A = circuits[a]
+    B = circuits[b]
+
+    A.add(a)
+    B.add(b)
+
+    if A != B:
+        C = A | B
+        circuits.update({n: C for n in C})
+
+        # one big circuit
+        if len(C) == n:
+            print(points[a][0] * points[b][0])
+            break
+
+# sets = {frozenset(s) for s in circuits.values()}
+# circuit_lengths = sorted([len(s) for s in sets], reverse=True)
+# product = prod(circuit_lengths[:3])
+
+# print(product)
